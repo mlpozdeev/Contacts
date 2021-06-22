6b52a739683c9e78c0e99412f9d8a45f13da6722
mlpozdeev <84346652+mlpozdeev@users.noreply.github.com>
2021-06-22 20:56:46 +0400

    Настроил цвета интерфейса, создал фрагмент для списка контактов

--- ContactsApp/app/src/main/java/com/mlpozdeev/contactsapp/presentation/MainActivity.kt
+++ ContactsApp/app/src/main/java/com/mlpozdeev/contactsapp/presentation/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.mlpozdeev.contactsapp
+package com.mlpozdeev.contactsapp.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.mlpozdeev.contactsapp.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
---
+++ ContactsApp/app/src/main/java/com/mlpozdeev/contactsapp/presentation/fragments/contacts/view/ContactsFragment.kt
@@ -0,0 +1,30 @@
+package com.mlpozdeev.contactsapp.presentation.fragments.contacts.view
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.mlpozdeev.contactsapp.R
+import com.mlpozdeev.contactsapp.databinding.FragmentContactsBinding
+
+class ContactsFragment : Fragment() {
+
+    private var _binding: FragmentContactsBinding? = null
+
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentContactsBinding.inflate(inflater, container, false)
+
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
